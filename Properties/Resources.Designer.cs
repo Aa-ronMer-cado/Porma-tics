b295ba38df31ac110f56f91bfc355839c3074ee1
kyli <mikylavc3@gmail.com>
2026-05-11 05:54:16 +0800

    Updating README

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -233,6 +233,16 @@ namespace Pormatics.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap UML {
+            get {
+                object obj = ResourceManager.GetObject("UML", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Upload {
             get {
                 object obj = ResourceManager.GetObject("Upload", resourceCulture);
